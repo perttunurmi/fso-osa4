738ca1a800f517bb8fb8d0d979984069757e8520
Perttu Nurmi <perttu.nurmi@gmail.com>
2025-08-04 17:21:20 +0300

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (sum, current) => sum + current.likes
+    , 0
+  )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
