ed20b200015a5c80c37d7492926ac4feb0763efb
Perttu Nurmi <perttu.nurmi@gmail.com>
2025-08-05 18:51:47 +0300

    4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -15,8 +15,38 @@ const favoriteBlog = (blogs) => {
   )
 }
 
+const howManyBlogs = (blogs, author) => {
+  return blogs.filter(blog => blog.author === author).length
+}
+
+
+// Palauttaa kirjoittajan, jolla on eniten blogeja.
+// Päähän sattui, mutta sain toteutettua ilman for-looppia!
+const mostBlogs = (blogs) => {
+
+  counts = blogs.map(
+    (blog) => {
+      author = {
+        author: blog.author,
+        count: howManyBlogs(blogs, blog.author)
+      }
+      return author
+    }
+  )
+
+  most = counts.reduce((most, curr) => curr.count > most.count ? curr : most
+    , counts[0])
+
+  return {
+    author: most.author,
+    blogs: most.count
+  }
+
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
